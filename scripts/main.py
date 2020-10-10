eefe6e266ef9aefe747d8b56c7b61c4a336ea9a1
xco1014 <61931420+xco1014@users.noreply.github.com>
2020-10-11 02:18:46 +0800

    V2

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,53 @@
 import RPi.GPIO as GPIO
 import time
 import sys
+import board
+import neopixel
+
+
+pixel_pin = board.D12
+ 
+# The number of NeoPixels
+num_pixels = 16
+ORDER = neopixel.GRB
+ 
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.5, auto_write=False, pixel_order=ORDER
+)
+ 
+ 
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+ 
+ 
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+ 
+
+
 
 amount  = float(sys.argv[1])
 
@@ -33,55 +80,59 @@ def chage(x):
         fertiliz(2)
         return
     elif x>=100:
-        ultimate()
+    	ultimate()
 
 def  ultimate():
-    print("ultimate")
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(CONTROL_PIN_1,GPIO.OUT)
-    GPIO.setup(CONTROL_PIN_2,GPIO.OUT)
-    GPIO.setup(CONTROL_PIN_4,GPIO.OUT)
-    pwm1 = GPIO.PWM(CONTROL_PIN_1, PWM_FREQ)
-    pwm2 = GPIO.PWM(CONTROL_PIN_2, PWM_FREQ)
-    pwm4 = GPIO.PWM(CONTROL_PIN_4, PWM_FREQ)
-    pwm1.start(0)
-    pwm2.start(0)
-    pwm4.start(0)
-    dc = angle_to_duty_cycle(sprayAct)
-    pwm1.ChangeDutyCycle(dc)
-    dc2 = angle_to_duty_cycle(ferAct)
-    pwm2.ChangeDutyCycle(dc2)
-    dc4 = angle_to_duty_cycle(rota)
-    pwm4.ChangeDutyCycle(dc4)
-    time.sleep(2)
+	print("ultimate")
+	rainbow_cycle(0.002)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(CONTROL_PIN_1,GPIO.OUT)
+	GPIO.setup(CONTROL_PIN_2,GPIO.OUT)
+	GPIO.setup(CONTROL_PIN_4,GPIO.OUT)
+	pwm1 = GPIO.PWM(CONTROL_PIN_1, PWM_FREQ)
+	pwm2 = GPIO.PWM(CONTROL_PIN_2, PWM_FREQ)
+	pwm4 = GPIO.PWM(CONTROL_PIN_4, PWM_FREQ)
+	pwm1.start(0)
+	pwm2.start(0)
+	pwm4.start(0)
+	dc = angle_to_duty_cycle(sprayAct)
+	pwm1.ChangeDutyCycle(dc)
+	dc2 = angle_to_duty_cycle(ferAct)
+	pwm2.ChangeDutyCycle(dc2)
+	dc4 = angle_to_duty_cycle(rota)
+	pwm4.ChangeDutyCycle(dc4)
+	time.sleep(2)
 
-    dc = angle_to_duty_cycle(sprayStop)
-    pwm1.ChangeDutyCycle(dc)
-    dc2 = angle_to_duty_cycle(ferStop)
-    pwm2.ChangeDutyCycle(dc2)
+	dc = angle_to_duty_cycle(sprayStop)
+	pwm1.ChangeDutyCycle(dc)
+	dc2 = angle_to_duty_cycle(ferStop)
+	pwm2.ChangeDutyCycle(dc2)
+
+	pixels.fill((0, 0, 0))
+	pixels.show()
     
-    time.sleep(1)
-    pwm1.stop()
-    pwm2.stop()
-    pwm4.stop()
-    GPIO.cleanup()
+	time.sleep(1)
+	pwm1.stop()
+	pwm2.stop()
+	pwm4.stop()
+	GPIO.cleanup()
 
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(CONTROL_PIN_3, GPIO.OUT)
-    pwm = GPIO.PWM(CONTROL_PIN_3, PWM_FREQ)
-    pwm.start(0)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(CONTROL_PIN_3, GPIO.OUT)
+	pwm = GPIO.PWM(CONTROL_PIN_3, PWM_FREQ)
+	pwm.start(0)
 
-    dc = angle_to_duty_cycle_9g(fer2Act)
-    pwm.ChangeDutyCycle(dc)
-    time.sleep(1)
-    dc = angle_to_duty_cycle_9g(fer2Stop)
-    pwm.ChangeDutyCycle(dc)
+	dc = angle_to_duty_cycle_9g(fer2Act)
+	pwm.ChangeDutyCycle(dc)
+	time.sleep(1)
+	dc = angle_to_duty_cycle_9g(fer2Stop)
+	pwm.ChangeDutyCycle(dc)
 
-    time.sleep(1)
-    pwm.stop()
-    GPIO.cleanup()
+	time.sleep(1)
+	pwm.stop()
+	GPIO.cleanup()
 
-    
+	
 
 def fertiliz(t):
     print("fertiliz")
